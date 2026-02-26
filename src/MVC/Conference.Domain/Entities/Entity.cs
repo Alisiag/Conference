d14d2e5c0648226963445297a031303881ce3e13
Alisiag <alisia.gubsk@gmail.com>
2026-02-26 22:15:45 +0200

    add base Entity class and IAggregateRoot interface

---
+++ src/MVC/Conference.Domain/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conference.Domain.Entities
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/MVC/Conference.Domain/Entities/IAggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conference.Domain.Entities
+{
+    public interface IAggregateRoot
+    {
+    }
+}
