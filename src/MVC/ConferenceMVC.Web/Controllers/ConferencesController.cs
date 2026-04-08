790614c243f4e1427260d3c3f62ee06e8d15ad95
Alisiag <alisia.gubsk@gmail.com>
2026-04-08 18:57:31 +0300

    initialize Web layer and inject DbContext

---
+++ src/MVC/ConferenceMVC.Web/Controllers/ConferencesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ConferenceMVC.Domain.Entities;
+using ConferenceMVC.Infrastucture;
+
+namespace ConferenceMVC.Web.Controllers
+{
+    public class ConferencesController : Controller
+    {
+        private readonly ConferenceContext _context;
+
+        public ConferencesController(ConferenceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Conferences
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Conferences.ToListAsync());
+        }
+
+        // GET: Conferences/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conference = await _context.Conferences
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (conference == null)
+            {
+                return NotFound();
+            }
+
+            return View(conference);
+        }
+
+        // GET: Conferences/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Conferences/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Description,StartDate,EndDate,Id")] Conference conference)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(conference);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(conference);
+        }
+
+        // GET: Conferences/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conference = await _context.Conferences.FindAsync(id);
+            if (conference == null)
+            {
+                return NotFound();
+            }
+            return View(conference);
+        }
+
+        // POST: Conferences/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Description,StartDate,EndDate,Id")] Conference conference)
+        {
+            if (id != conference.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(conference);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ConferenceExists(conference.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(conference);
+        }
+
+        // GET: Conferences/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var conference = await _context.Conferences
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (conference == null)
+            {
+                return NotFound();
+            }
+
+            return View(conference);
+        }
+
+        // POST: Conferences/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var conference = await _context.Conferences.FindAsync(id);
+            if (conference != null)
+            {
+                _context.Conferences.Remove(conference);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ConferenceExists(int id)
+        {
+            return _context.Conferences.Any(e => e.Id == id);
+        }
+    }
+}
